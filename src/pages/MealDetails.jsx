379a2ad9dd2feed96fcba65d0e7615a95c9ebf49
Pawel Walicki <pawel.walicki89@interia.pl>
2024-11-23 13:25:53 +0100

    new function and css

--- src/pages/MealDetails.jsx
+++ src/pages/MealDetails.jsx
@@ -1,5 +1,7 @@
+import { Grid2, Button, ImageList } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom";
+import './MealDetails.css'
 
 export const MealDetails = () => {
     let { mealId } = useParams()
@@ -25,10 +27,13 @@ export const MealDetails = () => {
         for (let i = 1; i <= 20; i++) {
             if (meal[`strIngredient${i}`]) {
                 jsx.push(
-                    <div>
-                        <img height={200} src={`https://www.themealdb.com/images/ingredients/${meal[`strIngredient${i}`]}.png`}></img>
-                        <div>{meal[`strIngredient${i}`]}: {meal[`strMeasure${i}`]}</div>
-                    </div>
+                    <Grid2 size={4}>
+                        <div>
+                            <img height={200} src={`https://www.themealdb.com/images/ingredients/${meal[`strIngredient${i}`]}.png`}></img>
+                            <div>{meal[`strIngredient${i}`]}: {meal[`strMeasure${i}`]}</div>
+                        </div>
+                    </Grid2>
+
                 )
             }
             else {
@@ -38,11 +43,56 @@ export const MealDetails = () => {
         return jsx
     }
 
+    const generateYtLink = (ytLink) => {
+        const videoId = ytLink?.split("=")[1]
+        const embededLink = "https://www.youtube.com/embed/" + videoId
+        return embededLink
+    }
+
     return (
-        <div>
-            {generateIngredients()}
-            <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY"/>
-            <pre>{meal.strInstructions}</pre>
-        </div>
+        <div className="mealDetails">
+            <Grid2 container spacing={2}>
+                <Grid2 size={6}>
+                    <div>
+                        <h1 className="nameMeal">{meal.strMeal}</h1>
+                        <img height={400} src={meal.strMealThumb}></img>
+                    </div>
+                </Grid2>
+                <Grid2 size={6}>
+                    <div>
+                        <p className="titleSections">Ingredients</p>
+                        <Grid2 container spacing={2}>
+                            {generateIngredients()}
+                        </Grid2>
+                    </div>
+                </Grid2>
+                <Grid2 size={12}>
+                    <iframe src={generateYtLink(meal.strYoutube)} />
+                    <p className="titleSections">Instructions</p>
+                    <pre style={{
+                        backgroundColor: "#f9f9f9",
+                        border: "1px solid #ddd",
+                        borderRadius: "5px",
+                        padding: "15px",
+                        fontSize: "16px",
+                        fontFamily: "Roboto Mono",
+                        lineHeight: "1.6",
+                        whiteSpace: "pre-wrap",
+                        wordWrap: "break-word",
+                        color: "#333",
+                        marginTop: "20px"
+                    }}>{meal.strInstructions}</pre>
+                </Grid2>
+            </Grid2>
+            <div className="buttonHome">
+                <Link to={"/"}>
+                    <Button variant="outlined" style={{
+                        color: "#000000",
+                        backgroundColor: "#a9c100"
+                    }}>Home</Button>
+                </Link>
+            </div>
+        </div >
+
     )
 }
